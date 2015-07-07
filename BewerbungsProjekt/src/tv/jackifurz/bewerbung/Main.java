2be9ae9dedd6091d175d89f318232efdb70b411a
lksbrm <lukas.briem@hotmail.de>
2015-07-07 12:51:36 +0200

    onEnable() onDisable()
    Adding default methods and a colored Console Message

--- BewerbungsProjekt/src/tv/jackifurz/bewerbung/Main.java
+++ BewerbungsProjekt/src/tv/jackifurz/bewerbung/Main.java
@@ -1,8 +1,29 @@
 package tv.jackifurz.bewerbung;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin
 {
+   /** Objects */
+	
+	public Server _SERVER = Bukkit.getServer();
+	public ConsoleCommandSender _CONSOLE = _SERVER.getConsoleSender();
+	
+   /**--END--*/
+	
+	
+	@Override
+	public void onEnable()
+   {
+	   _CONSOLE.sendMessage("§4Lobby§fTools §6succesfully §aactivated!");
+   }
    
+   @Override
+   public void onDisable()
+   {
+	   _CONSOLE.sendMessage("§4Lobby§fTools §6succesfully §4stopped!!");
+   }
 }
